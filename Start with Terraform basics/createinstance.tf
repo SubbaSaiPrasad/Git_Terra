3ef8052675790646984a401e3a5d2f3943c5870a
sai <sai123@gmail.com>
2024-06-07 16:18:22 +0530

    commit

--- Start with Terraform basics/createinstance.tf
+++ Start with Terraform basics/createinstance.tf
@@ -1,6 +1,6 @@
 provider aws{
-    access_key = "Access_key"
-    secret_key = "SECRET_KEY"
+    access_key = "AKIAUGNPXWLPA7P6QSUN"
+    secret_key = "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"
     region = "us-east-2"
 }
 resource "aws_instance" "my_ec2_instance" {
