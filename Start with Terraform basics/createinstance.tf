070e6487ab2b19e8249b236b6a3e42aa26f8d0de
sai <sai123@gmail.com>
2024-06-07 15:38:13 +0530

    commit 1

--- Start with Terraform basics/createinstance.tf
+++ Start with Terraform basics/createinstance.tf
@@ -3,7 +3,6 @@ provider aws{
     secret_key = "ouz9s/Nlrn6AHbdS7BSNaiRRF4R+nevvR/YwIxAf"
     region = "us-east-2"
 }
-
 resource "aws_instance" "my_ec2_instance" {
     ami = "ami-05803413c51f242b7"
     instance_type = "t2.micro"
